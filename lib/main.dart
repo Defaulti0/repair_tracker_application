2144a9566ea820494eefdeb2694a8eb843bcc577
SDefaultio <crismartin994@gmail.com>
2024-02-20 13:55:07 -0500

    implemented dialog box for deleting employee

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Supabase.initialize(
     url: 'https://novdxmvytymxsrxxifcl.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vdmR4bXZ5dHlteHNyeHhpZmNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc2ODk0NjQsImV4cCI6MjAyMzI2NTQ2NH0.-nbWSqTM6_DdbPbhXqRsmUsoIzDnpmn5H42sklNSIJ0',
   );
+  
   runApp(const MyApp());
 }
 
--- lib/pages/employee_manage_pages/list_employees.dart
+++ lib/pages/employee_manage_pages/list_employees.dart
@@ -8,13 +8,16 @@ class ListEmployees extends StatefulWidget {
   State<ListEmployees> createState() => _ListEmployeesState();
 }
 
-class _ListEmployeesState extends State<ListEmployees> {
+class _ListEmployeesState extends State<ListEmployees> {  
   final _future = Supabase.instance.client
       .from('Employees')
       .select();
-
+  
+  
   @override
   Widget build(BuildContext context) {
+    final supabase = Supabase.instance.client;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Employee Management'),
@@ -44,16 +47,50 @@ class _ListEmployeesState extends State<ListEmployees> {
                       onPressed: () {
                         // change Text childs to textfields, change the delete button to confirmation icon
                       },
-                      icon: const Icon(Icons.delete),
+                      icon: const Icon(Icons.done),
                     ),
-                    title: Text('${employee['First Name']} " " ${employee['Last Name']}'),
+                    title: Text('${employee['First Name']} ${employee['Last Name']}'),
                     subtitle: Text('${employee['Employee ID'].toString()} \n ${employee['Employee Type']}'),
                     isThreeLine: true,
                     trailing: IconButton.outlined(
-                      onPressed: () {
+                      onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) => Dialog(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text("Are you sure you want to delete this employee?"),
+                                const SizedBox(height: 15),
+                                TextButton(
+                                  onPressed: () {
+                                    const SnackBar(
+                                      content: Text('Deleted Employee.'),
+                                    );
+                                    // await supabase
+                                    //   .from('Employees')
+                                    //   .delete()
+                                    //   .match({'Employee ID': 1234});
+                                    Navigator.pop(context);
+                                  }, 
+                                child: const Text('Yes')
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    
+                                    Navigator.pop(context);
+                                  }, 
+                                child: const Text('No'),
+                                )
+                              ],
+                            )
+                          )
+                        )
                         // Add Dialog for confirmation to delete
-                      },
-                      icon: const Icon(Icons.done)
+                      ),
+                      icon: const Icon(Icons.delete)
                     ),
                   ),
                 ],
